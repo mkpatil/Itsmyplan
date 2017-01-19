6782e851b0f2227935be2a7127cac565d17f54bc
Mahesh Patil <mahesh.patil2403@gmail.com>
2017-01-19 22:35:23 +0530

    Initial commit

---
+++ Itsmyplan/Itsmyplan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Itsmyplan
+//
+//  Created by Mahesh Patil on 19/01/17.
+//  Copyright © 2017 Itsmyplan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
